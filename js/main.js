bad76ba8b8647f5f4854ffb9d2935186032b2605
Tejas099 <60440155+Tejas099@users.noreply.github.com>
2022-10-08 16:19:24 +0530

    Feature : Adding Home Section (#1)
    * Home page
    
    Title, Description and Nav Bar
    
    * Removing unnecessary code
    
    * Created the Home Page
    
    Made the Home page response and added the responsive navigation bar to it.
    
    * Responsive Nav Bar with Active State
    
    Added responsiveness to nav bar and added te active state.
    
    * PR comment Resolved
    
    * Code Cleanup for Home Nav
    
    Co-authored-by: nikhil-mandlik <nikhilmandlik@sharechat.co>
    Co-authored-by: thetechteam-dev <110727118+thetechteam-dev@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("hello from main.js")
+function Navbar() {
+  var navBarElement = document.getElementById("nav-bar");
+  var navIconElement = document.getElementById("nav-icon");
+  if (navBarElement.className === "nav-bar") {
+    navBarElement.className += " responsive";
+    navIconElement.className += " resp";
+    document.getElementById("icon-image").src = "./res/img/x.svg";
+  } else {
+    navBarElement.className = "nav-bar";
+    navIconElement.className = "navigations icon";
+    document.getElementById("icon-image").src = "./res/img/menu.svg";
+  }
+}
+
+var header = document.getElementById("nav-bar");
+var nav = header.getElementsByClassName("navigations");
+for (var i = 0; i < nav.length; i++) {
+  nav[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    console.log(current);
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
